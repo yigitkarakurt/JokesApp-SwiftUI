a66d4e60e8b647f46cd7c0738ee912682b9ae370
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-04-03 13:30:42 +0300

    Initial Commit

---
+++ JokesApp-SwiftUI/JokesApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp_SwiftUIApp.swift
+//  JokesApp-SwiftUI
+//
+//  Created by Yigit Karakurt on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
